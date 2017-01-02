dd1a30be3a3fd510c9c25139b97fc6e7cc9616fb
Jake Mauk <larcondos@yahoo.com>
2017-01-02 12:47:59 -0500

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,39 +2,10 @@
 
 using namespace std;
 
-/*void add(double, double);
-void subtract(double, double);
-void multiply(double, double);
-void divide(double, double); */
-
-int dblresult;
-
-
-template<class T>
-T add(T x, T y)
-{
-    return x + y;
-}
-template<class T>
-T subtract(T x, T y)
-{
-    return x - y;
-}
-template<class T>
-T multiply(T x, T y)
-{
-    return x * y;
-}
-template<class T>
-T divide(T x, T y)
-{
-    return x / y;
-}
-
-
 int main()
 {
-    double dblNum1, dblNum2;
+
+    double dblNum1, dblNum2, dblresult;
     int intOption;
 
     do
@@ -54,91 +25,51 @@ int main()
             cout << "An invalid option was selected, try again.";
             continue;
         }
+
         if (intOption == 0)
-            break;
+            {
+                break;
+            }
 
             cout << "Please select 2 numbers seperated by a space: ";
             cin >> dblNum1 >> dblNum2;
 
+
             if (intOption == 1)
             {
-                cout << "The result of the addition is: "
-                      << add(dblNum1, dblNum2);
+                dblresult = dblNum1 + dblNum2;
+                cout << "You have selected Addition.\n";
+                cout << "The answer is: " << dblresult;
             }
             else if (intOption == 2)
             {
-                cout << "The result of the subtraction is: "
-                      << subtract(dblNum1, dblNum2);
+                dblresult = dblNum1 - dblNum2;
+                cout << "You have selected Subtraction.\n";
+                cout << "The answer is: " << dblresult;
             }
             else if (intOption == 3)
             {
-                cout << "The result of the multiplication is: "
-                      << multiply(dblNum1, dblNum2);
+                dblresult = dblNum1 * dblNum2;
+                cout << "You have selected Multiplication.\n";
+                cout << "The answer is: " << dblresult;
             }
             else if (intOption == 4)
             {
+
                 if (dblNum2 == 0)
-                {
-                    cout << "You can't divide by zero. Nice try, nerd.";
+                    cout << "You can't divide by zero. Nice try.";
                     continue;
-                }
-                else
-                    cout << "The result of the division is: "
-                      << divide(dblNum1, dblNum2);
+
+                dblresult = dblNum1 / dblNum2;
+                cout << "You have selected Division.\n";
+                cout << "The answer is: " << dblresult;
             }
             else
                 cout << "Invalid option, try again.";
 
-    } while(1);
 
+
+
+    } while(1);
     return 0;
 }
-/*
-void add(double x, double y)
-{
-    double dblresult;
-
-    dblresult = x + y;
-    cout << "You have selected Addition.\n";
-    cout << "The answer is: " << dblresult;
-    return;
-}
-
-void subtract(double x, double y)
-{
-    double dblresult;
-
-    dblresult = x - y;
-    cout << "You have selected Subtraction.\n";
-    cout << "The answer is: " << dblresult;
-    return;
-}
-template<class T>
-T add(T x, T y)
-{
-    return x + y;
-}
-void multiply(double x, double y)
-{
-    double dblresult;
-
-    dblresult = x * y;
-    cout << "You have selected Multiplication.\n";
-    cout << "The answer is: " << dblresult;
-    return;
-}template<class T>
-T add(T x, T y)
-{
-    return x + y;
-}
-
-void divide(double x, double y)
-{
-    double dblresult;
-
-    dblresult = x / y;
-    cout << "You have selected Division.\n";
-    cout << "The answer is: " << dblresult;
-    return;
-}
-*/
